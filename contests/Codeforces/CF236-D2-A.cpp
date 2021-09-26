61076a6bf96881de68416d842698ea4b1d757653
vpeopleonatank <vpeopleonatank@gmail.com>
2021-09-26 16:05:39 +0700

    update

--- contests/Codeforces/CF236-D2-A.cpp
+++ contests/Codeforces/CF236-D2-A.cpp
@@ -11,6 +11,15 @@ using namespace std;
 int main()
 {
     fastio;
-    
+    set<char> chars;
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++) {
+      chars.insert(s[i]);
+    }
+    if(chars.size() % 2 == 0)
+      cout << "CHAT WITH HER!";
+    else
+      cout << "IGNORE HIM!";
     return 0;
 }
