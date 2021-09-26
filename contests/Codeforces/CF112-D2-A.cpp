3fe2049851b1b4d258c49c3b9a8fcfd81f797b13
vpeopleonatank <vpeopleonatank@gmail.com>
2021-09-26 16:00:22 +0700

    update

--- contests/Codeforces/CF112-D2-A.cpp
+++ contests/Codeforces/CF112-D2-A.cpp
@@ -2,5 +2,19 @@
 
 using namespace std;
 
-int main() {}
+int solve() {
+  string a, b;
+  cin >> a >> b;
+  for (int i = 0; i < a.length(); ++i) {
+    if(tolower(a[i]) < tolower(b[i]))
+      return -1;
+    else if (tolower(a[i]) > tolower(b[i])) 
+      return 1;
+  }
+  return 0;
+}
+
+int main() {
+  cout << solve();
+}
 
