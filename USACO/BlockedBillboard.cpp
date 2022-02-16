95d0d85e8c34a145bcf0af16eb5385e165cd82e1
vpeopleonatank <vpeopleonatank@gmail.com>
2022-02-16 21:07:47 +0700

    update

---
+++ USACO/BlockedBillboard.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void setIO(string s, int inputFromFile) {
+  if (inputFromFile) {
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+  }
+}
+
+int main() {
+  setIO("billboard", 0);
+  int b1_x, b1_y, b2_x, b2_y, t_x, t_y;
+  cin >> b1_x >> b1_y >> b2_x >> b2_y >> t_x >> t_y;
+
+
+  return 0;
+}
