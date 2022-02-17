d9d620a7cb566c0e8a0073734e088b0d0aaab5b8
vpeopleonatank <vpeopleonatank@gmail.com>
2022-02-17 21:08:45 +0700

    update

--- USACO/BlockedBillboard.cpp
+++ USACO/BlockedBillboard.cpp
@@ -10,10 +10,24 @@ void setIO(string s, int inputFromFile) {
 }
 
 int main() {
-  setIO("billboard", 0);
-  int b1_x, b1_y, b2_x, b2_y, t_x, t_y;
-  cin >> b1_x >> b1_y >> b2_x >> b2_y >> t_x >> t_y;
-
+  setIO("billboard", 1);
+  int ll_x_b1, ll_y_b1, tr_x_b1, tr_y_b1, ll_x_b2, ll_y_b2, tr_x_b2, tr_y_b2,
+      ll_x_t, ll_y_t, tr_x_t, tr_y_t;
+  cin >> ll_x_b1 >> ll_y_b1 >> tr_x_b1 >> tr_y_b1 >> ll_x_b2 >> ll_y_b2 >>
+      tr_x_b2 >> tr_y_b2 >> ll_x_t >> ll_y_t >> tr_x_t >> tr_y_t;
+  int int_x_1 = min(tr_x_b1, tr_x_t) - max(ll_x_b1, ll_x_t);
+  int int_y_1 = min(tr_y_b1, tr_y_t) - max(ll_y_b1, ll_y_t);
+  int intersection1 = (int_x_1) * (int_y_1);
+  int int_x_2 = min(tr_x_b2, tr_x_t) - max(ll_x_b2, ll_x_t);
+  int int_y_2 = min(tr_y_b2, tr_y_t) - max(ll_y_b2, ll_y_t);
+  int intersection2 = (int_x_2) * (int_y_2);
+  int b_sum = (tr_x_b1 - ll_x_b1) * (tr_y_b1 - ll_y_b1) +
+              (tr_x_b2 - ll_x_b2) * (tr_y_b2 - ll_y_b2);
+  if (int_x_1 > 0)
+    b_sum -= intersection1;
+  if (int_x_2 > 0)
+    b_sum -= intersection2;
+  cout << b_sum;
 
   return 0;
 }
