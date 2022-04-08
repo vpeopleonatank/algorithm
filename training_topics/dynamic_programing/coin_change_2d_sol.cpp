a8de709b47a9e156b4e93c9cd141b46c798668e4
vpeopleonatank <vpeopleonatank@gmail.com>
2022-04-08 15:51:56 +0700

    update

---
+++ training_topics/dynamic_programing/coin_change_2d_sol.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+  fastio;
+  int n, m;
+  cin >> n >> m;
+  long long c[m];
+  for (int i = 0; i < m; ++i) {
+    cin >> c[i];
+  }
+  long long f[m + 1][n + 1];
+
+  return 0;
+}
